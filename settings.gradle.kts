0898a67918587c084eb015708e0c427f999e0a46
Moriarte228 <t.vyshnikova@gmail.com>
2026-05-13 00:00:42 +0200

    changed settings

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,27 +1,19 @@
+rootProject.name = "WeatherCMP"
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+
 pluginManagement {
     repositories {
-        google {
-            content {
-                includeGroupByRegex("com\\.android.*")
-                includeGroupByRegex("com\\.google.*")
-                includeGroupByRegex("androidx.*")
-            }
-        }
+        google()
         mavenCentral()
         gradlePluginPortal()
     }
 }
-plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
-}
+
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
     }
 }
 
-rootProject.name = "task9"
-include(":app")
- 
+include(":composeApp")
