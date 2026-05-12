282f71b2d8edd7a2509753edb3dcaa490b55b0aa
Moriarte228 <t.vyshnikova@gmail.com>
2026-05-12 23:41:01 +0200

    added important files

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,19 +1,27 @@
-rootProject.name = "WeatherCMP"
-enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
-
 pluginManagement {
     repositories {
-        google()
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
         mavenCentral()
         gradlePluginPortal()
     }
 }
-
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+}
 dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
     }
 }
 
-include(":composeApp")
+rootProject.name = "task9"
+include(":app")
+ 
